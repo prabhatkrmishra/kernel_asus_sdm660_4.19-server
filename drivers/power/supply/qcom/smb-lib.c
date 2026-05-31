e809a40087bde0ac95e50965c39373b3bb02859a
ris <rissu.ntk@gmail.com>
2026-05-31 08:34:28 +0700

    ARM64: boot/dts: {X00TD, X01BD}, power: Tune charging speed limits
    * Adapted from https://github.com/Atom-X-Devs/scarlet_xiaomi_sdm660/commit/77fae06af0a5aa5af8a43ac58d116953ef85d850
    
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: I0aa63fd71d42a912313a5e784ff3d022d2d6aaa2
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2786,19 +2786,11 @@ int smblib_get_prop_die_health(struct smb_charger *chg,
 	return 0;
 }
 
-#ifdef CONFIG_MACH_ASUS_SDM660
-#define SDP_CURRENT_UA			3300000
-#define CDP_CURRENT_UA			3300000
-#define DCP_CURRENT_UA			3300000
-#define HVDCP_CURRENT_UA		3300000
-#define TYPEC_HIGH_CURRENT_UA		3300000
-#else
 #define SDP_CURRENT_UA			500000
 #define CDP_CURRENT_UA			1500000
-#define DCP_CURRENT_UA			1500000
+#define DCP_CURRENT_UA			2000000
 #define HVDCP_CURRENT_UA		3000000
 #define TYPEC_HIGH_CURRENT_UA		3000000
-#endif
 
 #define TYPEC_DEFAULT_CURRENT_UA	900000
 #define TYPEC_MEDIUM_CURRENT_UA		1500000
@@ -4017,11 +4009,7 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, false, 0);
 		break;
 	case POWER_SUPPLY_TYPE_USB_CDP:
-#ifdef CONFIG_MACH_ASUS_SDM660
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
-#else
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 1500000);
-#endif
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, CDP_CURRENT_UA);
 		break;
 	case POWER_SUPPLY_TYPE_USB_DCP:
 #ifndef CONFIG_MACH_ASUS_SDM660
@@ -4029,7 +4017,7 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		rp_ua = get_rp_based_dcp_current(chg, typec_mode);
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, rp_ua);
 #else
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
+		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, DCP_CURRENT_UA);
 #endif
 		break;
 	case POWER_SUPPLY_TYPE_USB_FLOAT:
@@ -4037,19 +4025,11 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		 * limit ICL to 100mA, the USB driver will enumerate to check
 		 * if this is a SDP and appropriately set the current
 		 */
-#ifdef CONFIG_MACH_ASUS_SDM660
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
-#else
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 100000);
-#endif
 		break;
 	case POWER_SUPPLY_TYPE_USB_HVDCP:
 	case POWER_SUPPLY_TYPE_USB_HVDCP_3:
-#ifdef CONFIG_MACH_ASUS_SDM660
-		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3300000);
-#else
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 3000000);
-#endif
 		break;
 	default:
 		smblib_err(chg, "Unknown APSD %d; forcing 500mA\n", pst);
